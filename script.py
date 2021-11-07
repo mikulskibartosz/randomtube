f4f3e18a1016c5bbcd75ba6226adff7795e16786
Bartosz Mikulski <mail@mikulskibartosz.name>
2021-11-07 11:17:23 +0100

    script and source data

---
+++ script.py
@@ -0,0 +1,44 @@
+import gzip
+import random
+import os
+
+with gzip.open('links.txt.gz', 'rt') as input_file:
+    lines = input_file.readlines()
+
+selected_articles = random.sample(lines, 1000)
+
+def turn_into_youtube_search_link(text):
+    no_new_line = text.strip()
+    underscore_to_plus = no_new_line.replace('_', '+')
+    underscore_to_space = no_new_line.replace('_', ' ')
+    return f'https://www.youtube.com/results?search_query={underscore_to_plus}', underscore_to_space
+
+youtube_links = map(turn_into_youtube_search_link, selected_articles)
+
+def generate_html(links):
+    def generate_link(link):
+        return f'<a href="{link[0]}" target="_blank">{link[1]}</a><br>'
+    
+    links_html = "\n".join(list(map(generate_link, links)))
+    
+    return f"""<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Random YouTube links</title>
+  </head>
+  <body>
+    <center>
+        {links_html}
+    </center>
+  </body>
+</html>
+    """
+
+html = generate_html(youtube_links)
+
+if not os.path.exists('public'):
+    os.mkdir('public')
+
+with open('public/index.html', 'w') as output_file:
+    output_file.write(html)
